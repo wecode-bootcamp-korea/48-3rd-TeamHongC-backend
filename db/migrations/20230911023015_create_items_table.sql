b0e5cd71963ea93ae39678f3d26055fdb7b5cbc9
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2023-09-11 21:14:20 +0900

    찐막

--- db/migrations/20230911023015_create_items_table.sql
+++ db/migrations/20230911023015_create_items_table.sql
@@ -8,7 +8,9 @@ CREATE TABLE items (
   title varchar(255) NOT NULL,
   price int NOT NULL,
   item_count int NOT NULL DEFAULT 1,
-  location varchar(255),
+  longitude decimal(25, 15) NOT NULL,
+  latitude decimal(25, 15) NOT NULL,
+  region text NOT NULL
   created_at timestamp NOT NULL DEFAULT current_timestamp,
   updated_at timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
   constraint items_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
