1e332bdfca49b452270714f0fdad8963b2805202
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2023-09-15 14:02:50 +0900

    내 주변 물품 get

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { detailRouter } = require('./itemDetail');
 const { userRouter } = require('./user.router');
+const { surroundItemRouter } = require('./surroundItems');
 
 const routes = express.Router();
 
 routes.use('/product-detail', detailRouter);
 routes.use('/user', userRouter);
+routes.use('/my', surroundItemRouter);
+
 module.exports = { routes };
---
+++ src/routes/surroundItems.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getSurroundItem } = require('../controllers/surroundItemController');
+const surroundItemRouter = express.Router();
+
+surroundItemRouter.get('/surround', getSurroundItem);
+
+module.exports = { surroundItemRouter };
