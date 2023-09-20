5538027a67ce800810fdb91f955600f5060c555d
ParkGeonRyul <qyskamslepf@gmail.com>
2023-09-20 18:43:10 +0900

    feature: 후기 리스트 기능 구현

--- src/services/itemDetailService.js
+++ src/services/itemDetailService.js
@@ -1,4 +1,4 @@
-const itemDetailDao = require('../models/itemDetailDao');
+const itemDetailDao = require("../models/itemDetailDao");
 
 const getItemDetail = async (id) => {
   try {
@@ -7,7 +7,7 @@ const getItemDetail = async (id) => {
       itemDetailDao.getItemImage(id),
     ]);
 
-    detail['price'] = detail['price'].toLocaleString() + '원';
+    detail["price"] = detail["price"].toLocaleString() + "원";
 
     const createdAt = detail.createdAt;
     const today = new Date();
@@ -15,33 +15,36 @@ const getItemDetail = async (id) => {
       (today.getTime() - createdAt.getTime()) / 1000 / 60
     );
     if (difference <= 1) {
-      detail['date'] = '방금전';
+      detail["date"] = "방금전";
     } else if (difference < 60) {
-      detail['date'] = `${difference}분전`;
+      detail["date"] = `${difference}분전`;
     }
 
     const differenceHour = Math.floor(difference / 60);
     if (differenceHour !== 0 && differenceHour < 24) {
-      detail['date'] = `${differenceHour}시간전`;
+      detail["date"] = `${differenceHour}시간전`;
     }
 
     const differenceTimeDay = Math.floor(difference / 60 / 24);
     if (differenceTimeDay !== 0 && differenceTimeDay < 7) {
-      detail['date'] = `${differenceTimeDay}일전`;
+      detail["date"] = `${differenceTimeDay}일전`;
     }
 
     const differenceMonthDay = Math.floor(difference / 60 / 24 / 30);
     if (differenceMonthDay !== 0 && differenceMonthDay < 12) {
-      detail['date'] = `${differenceMonthDay}개월 전`;
+      detail["date"] = `${differenceMonthDay}개월 전`;
     }
 
     delete detail.createdAt;
-    detail['image'] = image;
+    detail["image"] = image;
     return detail;
   } catch (err) {
-    console.log('An error occurred: ', err);
+    console.log("An error occurred: ", err);
     throw err;
   }
 };
 
-module.exports = { getItemDetail };
+const getReview = async (itemId) => {
+  return await itemDetailDao.getItemReview(itemId);
+};
+module.exports = { getItemDetail, getReview };
